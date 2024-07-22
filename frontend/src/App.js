ff6063f21a87ae7d3150035db261040410bbd7e0
Hosni Yahyaoui <50012939+hosniyahyaoui@users.noreply.github.com>
2024-07-22 16:35:44 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,24 +44,8 @@ function App() {
   });
   useEffect(()=>{
       (function(d, m){
-        var kommunicateSettings = 
-            {"appId":"1d4a29b9a0037251fbe73ad229b113851","popupWidget":true,"automaticChatOpenOnNavigation":true,
-        
-  "automaticChatOpenOnNavigation":true,
-             "voiceOutput":true,"voiceInput":true,"emojilibrary":true,
-             "voiceName":"Microsoft David Desktop - English (United States)", // Replace Google Deutsch with the voiceName or an array of voiceNames from the below mentioned table list
-             "voiceRate":0,"locShare":true
-
-
-      ,  
-        };
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-         kommunicateSettings.restartConversationByUser = true;
-           
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-        window.kommunicate = m; m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
+        var kommunicateSettings = "api key kommunicate"
+            
 
 
   },[])
